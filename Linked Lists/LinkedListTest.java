a89a68de73cbb50d8afb402d1bdabbc37ecb55ab
Prodigy <jjiemjitpolchai9540@bths.edu>
2017-04-06 09:46:06 -0400

    Update LinkedListTest.java

--- Linked Lists/LinkedListTest.java
+++ Linked Lists/LinkedListTest.java
@@ -8,12 +8,21 @@ public class LinkedListTest {
 		LinkNode node4 = new LinkNode("Like");
 		LinkNode node5 = new LinkNode("Cats");
 		
+		/*node1.setNext(node2);
+		node2.setNext(node3);
+		node3.setNext(node4);
+		node4.setNext(node5);*/
+		
 		SinglyLinkedList list = new SinglyLinkedList(node1);
 		list.add(list, "World");
 		list.add(list, "I");
 		list.add(list, "Like");
 		list.add(list, "Cats");
 		
+		System.out.println(list.search(list, "Hello"));
+		System.out.println(list.search(list, "World"));
+		System.out.println(list.search(list, "I"));
+		System.out.println(list.search(list, "Like"));
 		System.out.println(list.search(list, "Cats"));
 	}
 

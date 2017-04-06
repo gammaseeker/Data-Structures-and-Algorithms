c3fedf36e87ebb3875b9679c81a70a5b2aaaf844
Prodigy <jjiemjitpolchai9540@bths.edu>
2017-04-06 09:45:44 -0400

    Update LinkNode.java

--- Linked Lists/LinkNode.java
+++ Linked Lists/LinkNode.java
@@ -1,12 +1,31 @@
 
 public class LinkNode {
 	
-	String data;
-	LinkNode next;
+	private String data;
+	private LinkNode next;
 	public LinkNode(String item)
 	{
 		data = item;
-		next = null;
+		
 	}
-
+	
+	public boolean setNext(LinkNode next)
+	{
+		this.next = next;
+		return true;
+	}
+	
+	public LinkNode getNext()
+	{
+		if(this.next != null)
+		{
+			return this.next;
+		}
+		return null;
+	}
+	
+	public String getData()
+	{
+		return data;
+	}
 }

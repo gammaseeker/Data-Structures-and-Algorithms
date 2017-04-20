759080a6716818d5ce233f0b8efe6abba7066c64
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-04-19 21:31:49 -0400

    Create RecursionPractice.java

---
+++ Recursion/RecursionPractice.java
@@ -0,0 +1,14 @@
+public class RecursionPractice
+{
+  public static void main(String[] args)
+  {
+    System.out.println(twoPow(3));
+  }
+  
+  public static int twoPow(int n) //2^n
+  {
+    if(n == 0)
+      return 1;
+    return 2 * twoPow(n - 1);
+  }
+}

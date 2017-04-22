90a0e22e9dbd0ea338468c2a157e54f96365b658
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-04-22 00:49:00 -0400

    Add files via upload

---
+++ linkedLists/DoublyLinkedList.java
@@ -0,0 +1,32 @@
+package linkedLists;
+
+public class DoublyLinkedList extends LinkedList
+{
+	public DoublyLinkedList()
+	{
+		super();
+	}
+	
+	public DoublyLinkedList(Node node)
+	{
+		super(node);
+	}
+	
+	@Override
+	public void add(Node node) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insert(Node node, int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+}
--- linkedLists/Node.java
+++ linkedLists/Node.java
@@ -3,6 +3,7 @@ package linkedLists;
 public class Node  {
 	private int data;
 	private Node next;
+	private Node prev;
 	public Node()
 	{
 		
@@ -12,12 +13,24 @@ public class Node  {
 	{
 		this.data = item;
 		this.next = null;
+		this.prev = null;
+	}
+	
+	public boolean setPrev(Node next)
+	{
+		this.prev = next;
+		return true;
 	}
 	
 	public boolean setNext(Node next)
 	{
 		this.next = next;
 		return true;
+	}
+	
+	public Node getPrev()
+	{
+		return this.prev;
 	}
 	
 	public Node getNext()

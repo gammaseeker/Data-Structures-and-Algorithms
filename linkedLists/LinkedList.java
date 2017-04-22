056a8abbd1c88decaf4caea997e555b3e2688433
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-04-22 00:03:49 -0400

    Update LinkedList.java

--- linkedLists/LinkedList.java
+++ linkedLists/LinkedList.java
@@ -10,11 +10,16 @@ abstract public class LinkedList
 		head = new Node();
 		length = 1;
 	}
+	
 	public LinkedList(Node node)
 	{
 		head = node;
 		length = 1;
 	}
+	/**
+	 * 
+	 * @return the length of the linked list
+	 */
 	public int size()
 	{
 		return length;
@@ -23,7 +28,7 @@ abstract public class LinkedList
      * 
      * @param list the SinglyLinkedList to search in.
      * @param target the int to search for.
-     * @return a node that is specified by @param target.
+     * @return a Node that is specified by @param target.
      */
     public Node search(int target)
     {
@@ -42,10 +47,28 @@ abstract public class LinkedList
     	}
     	return null;
     }
-    
+    /**
+    * Appends node to end of linked list
+	* @param node the node to be added.
+	*/
 	public abstract void add(Node node);
+	
+	/**
+	 * Inserts node at specified index.
+	 * @param node the node to be inserted
+	 * @param index the position of where to insert the node
+	 */
 	public abstract void insert(Node node, int index);
+	
+	/**
+	 * Deletes node at index
+	 * @param index the position of the node to be deleted
+	 */
 	public abstract void delete(int index);
+	
+	/**
+	 * Displays all data in linked list in a forward manner
+	 */
 	public void display()
 	{
 		Node x = head;

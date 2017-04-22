2e4909d50d06f1eae87757bd147fe4f40eff5079
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-04-22 00:00:51 -0400

    Update Test.java

--- linkedLists/Test.java
+++ linkedLists/Test.java
@@ -9,7 +9,9 @@ public class Test {
 		Node node4 = new Node(4);
 		Node node5 = new Node(5);
 		Node node6 = new Node(6);
-		
+		Node node7 = new Node(7);
+		Node node8 = new Node(8);
+		Node node9 = new Node(9);
 		SinglyLinkedList list = new SinglyLinkedList(node1);
 		list.add(node2);
 		list.add(node3);
@@ -23,11 +25,9 @@ public class Test {
 		list.display();
 		System.out.println();
 		
-		SinglyLinkedList list2 = new SinglyLinkedList(node6);
-		list2.add(node2);
-		list2.add(node3);
-		list2.add(node4);
-		list2.add(node5);
+		SinglyLinkedList list2 = new SinglyLinkedList(node7); //Cannot reuse nodes in other linked lists, the objects retain their properties from previous mutations
+		list2.add(node8);
+		list2.add(node9);
 		list2.display();
 
 	}

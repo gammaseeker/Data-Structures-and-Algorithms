c6d909188562b1b306cdec27e746bce0560d7639
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-04-24 22:58:31 -0400

    Add files via upload

---
+++ tries/BinarySearchTree.java
@@ -0,0 +1,20 @@
+package tries;
+
+public class BinarySearchTree {
+	private Node root;
+	
+	public BinarySearchTree()
+	{
+		
+	}
+	
+	public BinarySearchTree(Node root)
+	{
+		this.root = root;
+	}
+	
+	public void insert(Node n)
+	{
+		
+	}
+}

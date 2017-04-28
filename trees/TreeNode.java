c7dd77a3ad2b3d9a5479e1550ba4ad580a220783
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-04-27 23:25:02 -0400

    Update TreeNode.java

--- trees/TreeNode.java
+++ trees/TreeNode.java
@@ -47,27 +47,7 @@ public class TreeNode {
 		return size;
 	}
 	
-	public boolean isBST() {
-		if (left != null) {
-			if (data < left.data || !left.isBST()) {
-				return false;
-			}
-		}
-		
-		if (right != null) {
-			if (data >= right.data || !right.isBST()) {
-				return false;
-			}
-		}		
-		
-		return true;
-	}
 	
-	public int height() {
-		int leftHeight = left != null ? left.height() : 0;
-		int rightHeight = right != null ? right.height() : 0;
-		return 1 + Math.max(leftHeight, rightHeight);
-	}
 	
 	public TreeNode find(int d) {
 		if (d == data) {
@@ -80,22 +60,5 @@ public class TreeNode {
 		return null;
 	}
 	
-	private static TreeNode createMinimalBST(int arr[], int start, int end){
-		if (end < start) {
-			return null;
-		}
-		int mid = (start + end) / 2;
-		TreeNode n = new TreeNode(arr[mid]);
-		n.setLeftChild(createMinimalBST(arr, start, mid - 1));
-		n.setRightChild(createMinimalBST(arr, mid + 1, end));
-		return n;
-	}
 	
-	public static TreeNode createMinimalBST(int array[]) {
-		return createMinimalBST(array, 0, array.length - 1);
-	}
-	
-	public void print() {
-		BTreePrinter.printNode(this);
-	}
 } 

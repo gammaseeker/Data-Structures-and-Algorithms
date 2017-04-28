de71da9d9bdc38d0dda81bb17b32466fc6d214c8
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-04-27 23:07:31 -0400

    Add files via upload

--- trees/Node.java
+++ trees/Node.java
@@ -2,12 +2,29 @@ package trees;
 
 public class Node {
 	private int data;
-	private Node left;
-	private Node right;
+	private Node left, right, parent;
 	
 	public Node(int data)
 	{
 		this.data = data;
+	}
+	
+	public void setLeft(Node n)
+	{
+		this.left = n;
+		if(n!= null)
+		{
+			n.parent = this;
+		}
+	}
+	
+	public void setRight(Node n)
+	{
+		this.right = n;
+		if(n!= null)
+		{
+			n.parent = this;
+		}
 	}
 	
 	public int getData()

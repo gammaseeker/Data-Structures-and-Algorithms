36643b95c26b5f33d421e6e109c18a5906dfc16d
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-05-02 22:19:38 -0400

    Add files via upload

---
+++ hashTable/HashTable.java
@@ -0,0 +1,30 @@
+package hashTable;
+
+public class HashTable {
+	
+	private int key;
+	public HashTable()
+	{
+		
+	}
+	
+	private int hash(int data)
+	{
+		return -1;
+	}
+	
+	public void insert(int data)
+	{
+		
+	}
+	
+	public void search(int data)
+	{
+		
+	}
+	
+	public void delete()
+	{
+		
+	}
+}

e0ae578074fe12fdd4fdc632a705e10c0cc33cb4
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-05-19 10:35:05 -0400

    Boop

--- trees/BinarySearchTree.java
+++ trees/BinarySearchTree.java
@@ -13,8 +13,44 @@ public class BinarySearchTree {
 		this.root = root;
 	}
 	
-	public void insert(Node n)
+	public void insert(int data)
 	{
-		
+		if(data <= root.getData())
+		{
+			if(root.getLeft() == null)
+			{
+				root.setLeft(new Node(data));
+			}
+			else
+			{
+				root.getLeft().insert(data);
+			}
+		}
+		else
+		{
+			if(root.getRight() == null)
+			{
+				root.setRight(new Node(data));
+			}
+			else
+			{
+				root.getRight().insert(data);
+			}
+		}
+	}
+	
+	public Node getRoot()
+	{
+		return root;
+	}
+	
+	public void inOrderTraverse(Node root)
+	{
+		if(root != null)
+		{
+			inOrderTraverse(root.getLeft());
+			System.out.println(root.getData());
+			inOrderTraverse(root.getRight());
+		}
 	}
 }

d9f8db004ecf0db4755b0a36b1903d44353c4112
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-05-29 20:19:13 -0400

    Add files via upload

---
+++ insertionSort/InsertionSort.java
@@ -0,0 +1,38 @@
+package insertionSort;
+
+public class InsertionSort {
+	public static void main(String[] args)
+	{
+		int[] nums = new int[100000];
+		for (int i = 0; i < nums.length; i++)
+			nums[i] = (int)(Math.random() * 101);
+		
+		long start = 0;
+		long end = 0;
+		
+		System.out.print("insertionSort milliseconds: ");
+		for (int i = 0; i < nums.length; i++)
+			nums[i] = (int)(Math.random() * 101);
+			
+		start = System.currentTimeMillis();
+		insertionSort(nums);
+		end = System.currentTimeMillis();
+		System.out.println(end - start);
+	}
+	
+	public static void insertionSort(int[] elements)
+	{
+		for (int j = 1; j < elements.length; j++)
+		{
+			int temp = elements[j];
+			int possibleIndex = j;
+			while (possibleIndex > 0 && temp < elements[possibleIndex - 1])
+			{
+				elements[possibleIndex] = elements[possibleIndex - 1];
+				possibleIndex--;
+			}
+			elements[possibleIndex] = temp;
+		}
+	}
+	
+}

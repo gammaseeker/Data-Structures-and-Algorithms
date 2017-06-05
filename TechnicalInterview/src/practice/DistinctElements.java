e0bb29f21c665cce0b58c7a8a09375e1a4d1215f
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-06-04 20:56:15 -0400

    Add files via upload

---
+++ TechnicalInterview/src/practice/DistinctElements.java
@@ -0,0 +1,57 @@
+package practice;
+
+public class DistinctElements {
+	public static void main(String[] args)
+	{
+		int[] test1 = {1, 5, 15, 20};
+		int[] test2 = {2, 5, 13, 30};
+		System.out.println(bruteForce(test1, test2));
+		System.out.println(optimized(test1, test2));
+		int[] test3 = {1, 5, 15, 20, 37};
+		int[] test4 = {2, 5, 13, 30, 32, 35, 37, 42};
+		System.out.println(bruteForce(test3, test4));
+		System.out.println(optimized(test3, test4));
+	}
+	
+	private static int bruteForce(int[] a, int[] b)// O(ab)
+	{
+		int ctr = 0;
+		for(int i = 0; i < a.length; i++)
+		{
+			for(int j = 0; j < b.length; j++)
+			{
+				if(a[i] == b[j])
+					ctr++;
+			}
+		}
+		return ctr;
+	}
+	
+	private static int optimized(int[] a, int[] b)//O(a+b)
+	{
+		int ctr = 0;
+		int i = 0;
+		int j = 0;
+		while(i < a.length && j < b.length)
+		{
+			if(a[i] == b[j])
+			{	
+				ctr++;
+				i++;
+				j++;
+			}
+			else
+			{
+				if(a[i] > b[j])
+				{
+					j++;
+				}
+				else
+				{
+					i++;
+				}
+			}
+		}
+		return ctr;
+	}
+}
---
+++ TechnicalInterview/src/practice/Sum8.java
@@ -0,0 +1,5 @@
+package practice;
+
+public class Sum8 {
+
+}

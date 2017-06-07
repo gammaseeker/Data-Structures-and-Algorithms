9c6d8d7beb254f61699a13a3664d8ced2433080f
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-06-06 21:59:25 -0400

    Add files via upload

---
+++ TechnicalInterview/src/practice/Zero.java
@@ -0,0 +1,15 @@
+package practice;
+import java.util.*;
+
+public class Zero {
+
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public static void zero(ArrayList<Integer> input)
+	{
+		
+	}
+}

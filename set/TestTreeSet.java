5144d2cc772cbe7f4b340e112c4b8341cffec559
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-06-10 14:49:01 -0400

    Annotated api methods

--- set/TestTreeSet.java
+++ set/TestTreeSet.java
@@ -13,22 +13,22 @@ public class TestTreeSet {
 		set.add("New York");
 		
 		TreeSet<String> treeSet = new TreeSet<>(set);
-		System.out.println("Sorted tree set: " + treeSet);
+		System.out.println("Sorted tree set: " + treeSet);//Sorted by compareTo
 		
 		System.out.println("\nfirst(): " + treeSet.first());
 		System.out.println("last(): " + treeSet.last());
 		System.out.println("headSet(\"New York\"): " +
-				treeSet.headSet("New York"));
+				treeSet.headSet("New York"));//return items before New York, excluding New York
 		System.out.println("tailSet(\"New York\"): " +
-				treeSet.tailSet("New York"));
+				treeSet.tailSet("New York"));//returns items after New York, including New York
 
 		// Use the methods in NavigableSet interface
-		System.out.println("lower(\"P\"): " + treeSet.lower("P"));
-		System.out.println("higher(\"P\"): " + treeSet.higher("P"));
-		System.out.println("floor(\"P\"): " + treeSet.floor("P"));
-		System.out.println("ceiling(\"P\"): " + treeSet.ceiling("P"));
-		System.out.println("pollFirst(): " + treeSet.pollFirst());
-		System.out.println("pollLast(): " + treeSet.pollLast());
+		System.out.println("lower(\"P\"): " + treeSet.lower("P"));//returns the largest element less than P
+		System.out.println("higher(\"P\"): " + treeSet.higher("P"));//returns the smallest element greater than P
+		System.out.println("floor(\"P\"): " + treeSet.floor("P"));//returns the largest element less than or equal to P
+		System.out.println("ceiling(\"P\"): " + treeSet.ceiling("P"));//returns the smallest element greater than or equal to P
+		System.out.println("pollFirst(): " + treeSet.pollFirst());//removes the first element in treeSet and returns the removed element
+		System.out.println("pollLast(): " + treeSet.pollLast());//removes the last element in treeSet and returns the removed element 
 		System.out.println("New tree set: " + treeSet);
 	}
 }

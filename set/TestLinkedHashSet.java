c69199bc82ad423422d757dcad5eea8c109f33d0
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-06-10 14:35:25 -0400

    LinkedLists but with sets

--- set/TestLinkedHashSet.java
+++ set/TestLinkedHashSet.java
@@ -4,6 +4,20 @@ import java.util.*;
 public class TestLinkedHashSet {
 	public static void main(String[] args)
 	{
+		Set<String> set = new LinkedHashSet<>();
 		
+		set.add("London");
+		set.add("Paris");
+		set.add("New York");
+		set.add("San Francisco");
+		set.add("Beijing");
+		set.add("New York");
+		
+		System.out.println(set);
+		
+		for(String element: set)
+		{
+			System.out.print(element.toLowerCase() + " ");
+		}
 	}
 }

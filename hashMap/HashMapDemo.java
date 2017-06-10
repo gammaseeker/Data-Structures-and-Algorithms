a8f72e47a04a65b8daa39faf3bba56e27230a3ff
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-06-10 14:31:22 -0400

    "We'll do a Hash Map implementation"

---
+++ hashMap/HashMapDemo.java
@@ -0,0 +1,10 @@
+package hashMap;
+import java.util.*;
+
+public class HashMapDemo {
+	public static void main(String[] args)
+	{
+		HashMap<String, Integer> h = new HashMap<String, Integer>();
+		
+	}
+}

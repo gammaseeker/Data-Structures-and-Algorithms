901352e0c2b3d1a93b5ce630086876f287f60b6e
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-06-10 19:36:35 -0400

    Add files via upload

---
+++ hashMap/CountOccurrenceOfWords.java
@@ -0,0 +1,41 @@
+package hashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+public class CountOccurrenceOfWords {
+	public static void main(String[] args) 
+	{
+		// Set text in a string
+		String text = "Good morning. Have a good class. " + "Have a good visit. Have fun!";
+	
+		// Create a TreeMap to hold words as key and count as value
+		Map<String, Integer> map = new TreeMap<>();
+	
+		 String[] words = text.split("[ \n\t\r.,;:!?(){]");
+		 for (int i = 0; i < words.length; i++) 
+		 {
+			 String key = words[i].toLowerCase();
+			
+			 if (key.length() > 0) 
+			 {
+				 if (!map.containsKey(key)) 
+				 {
+					 map.put(key, 1);
+				 }
+				 else 
+				 {
+					 int value = map.get(key);
+					 value++;
+					 map.put(key, value);
+				 }
+			 }
+		 }
+		
+		 // Get all entries into a set
+		 Set<Map.Entry<String, Integer>> entrySet = map.entrySet();
+		 // Get key and value from each entry
+		 for (Map.Entry<String, Integer> entry: entrySet)
+		 System.out.println(entry.getValue() + "\t" + entry.getKey());
+	 }
+}
+

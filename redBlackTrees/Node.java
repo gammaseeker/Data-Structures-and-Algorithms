03b1396ccf716988e23aeac2c441be823a3cf4d8
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-09-21 17:00:24 -0400

    Constructors and fields

--- redBlackTrees/Node.java
+++ redBlackTrees/Node.java
@@ -1,5 +1,14 @@
 package redBlackTrees;
 
-public class Node {
 
+public class Node {
+	private int data;
+	private Node right;
+	private Node left;
+	private Node parent;
+	
+	public Node(int data)
+	{
+		this.data = data;
+	}
 }
